4bddfac4764d7918cf462dcaa3fc611965186795
3049255 <3049255@MC266459.ifrn.local>
2018-11-05 08:21:25 -0300

    aluno controller

---
+++ bliospring/src/controllers/AlunoController.java
@@ -0,0 +1,31 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AlunoController {
+	
+	}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
