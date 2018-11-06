685f0d85894af59ebeefb8eb0e70787127333d08
3049255 <3049255@MC391807.ifrn.local>
2018-11-06 16:47:59 -0200

    criando os aspectos de listagem de aluno

--- bliospring/src/controllers/AlunoController.java
+++ bliospring/src/controllers/AlunoController.java
@@ -1,7 +1,14 @@
 package controllers;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import dao.AlunoDAO;
+import models.Aluno;
 
 @Controller
 public class AlunoController {
@@ -10,6 +17,21 @@ public class AlunoController {
 		System.out.println("Chamou o form de aluno");
 		return "aluno/form";
 	}
+	
+	@GetMapping("/aluno")
+	public ModelAndView listar () {
+		AlunoDAO alunodao = new AlunoDAO();
+		List<Aluno> listand = alunodao.getLista();	
+			ModelAndView model = new ModelAndView("aluno/lista");	
+	model.addObject("alunos", listand);
+	return model;
+	}
+	
+	
+	
+	
+	
+	
 	}
 	
 
