21282ee087d373cbbdd5c4c78de63fba1d5eb6eb
3049255 <3049255@MC266459.ifrn.local>
2018-11-13 13:34:03 -0300

    conection change

--- bliospring/src/dao/ConnectionFactory.java
+++ bliospring/src/dao/ConnectionFactory.java
@@ -9,7 +9,7 @@ public class ConnectionFactory {
 	private static Connection connection = null;
 	private static String url = "jdbc:mysql://localhost/azul";
 	private static String user = "root";
-	private static String senha = "ti72";
+	private static String senha = "lab02";
 	
 	public static Connection getConnection() {
 		if (connection == null) {
